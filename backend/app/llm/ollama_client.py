6c859a86b360296e8e62f7e507eb1fade7872a72
Liang Chun Li <liangchun13124@gmail.com>
2025-12-11 01:07:35 +0800

    Feat: Add Ollama client support and secure API key config (#4)
    * Feat: Add Ollama client support and secure API key config
    
    * delete error syntax
    
    * fix var API name
    
    * change order to check error msg
    
    * stop at build basic connection
    
    * fix basic connection
    
    * Delete .vscode/settings.json
    
    should be block
    
    * Update .gitignore
    
    revise
    
    * Update .gitignore
    
    revise typo
    
    ---------
    
    Co-authored-by: vanillaSky00 <148969978+vanillaSky00@users.noreply.github.com>

--- backend/app/llm/ollama_client.py
+++ backend/app/llm/ollama_client.py
@@ -1,11 +1,42 @@
+from typing import Type, TypeVar
+from pydantic import BaseModel
+# from langchain_community.chat_models import ChatOllama
+from langchain_ollama import ChatOllama
 from .base import BaseLLMClient
 
+T = TypeVar("T", bound=BaseModel)
+
 class OllamaClient(BaseLLMClient):
-    def __init__(self, api_key: str, model: str = ""):
-        pass
-        
+    def __init__(self, base_url: str, model: str = "gemma3:4b", api_key: str = None):
+
+        headers = {}
+        if api_key:
+            headers["Authorization"] = f"Bearer {api_key}"
+            #print(f"[DEBUG] Final Headers: {headers}")
+
+        self.llm = ChatOllama(
+            base_url=base_url,
+            model=model,
+            #temperature=0.7,
+            client_kwargs={
+                "headers": headers
+            }
+        )
+
     async def generate_response(self, system_prompt: str, user_message: str) -> str:
-        pass
-    
+        messages = [
+            ("system", system_prompt),
+            ("human", user_message)
+        ]
+        response = await self.llm.ainvoke(messages)
+        return response.content
+
     async def generate_structured(self, system_prompt: str, user_message: str, response_model: Type[T]) -> T:
-        pass
+
+        structured_llm = self.llm.with_structured_output(response_model)
+        
+        messages = [
+            ("system", system_prompt),
+            ("human", user_message)
+        ]
+        return await structured_llm.ainvoke(messages)
